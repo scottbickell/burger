dc215a065ff15aa2952d6ab34204a2fabc453a52
Scott Bickell <bickellworks@gmail.com>
2019-02-03 20:47:51 -0700

    Add project files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Kahuna Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Elk Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salmon Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", false);
